6edee1274a1def3b8a889e4193c0e283336ab9fe
Gopi Kumar <gopi@Gopis-Mac-mini.local>
2022-08-06 19:30:21 +0530

    add variable for srvtwo URL

--- main.go
+++ main.go
@@ -20,14 +20,18 @@ func main() {
 
 	fmt.Println("Listening on port", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Panic("Error on startup", err)
+		log.Fatal("Error on startup", err)
 	}
 }
 
 func SrvOne(w http.ResponseWriter, r *http.Request) {
+	srvTwoURL := os.Getenv("SERVICE_TWO_URL")
+	if srvTwoURL == "" {
+		log.Fatal("Service two URL missing !\n")
+	}
 	if r.Method == "GET" {
 		//fmt.Fprintf(w, "Welcome to service one !\n")
-		resp, err := http.Get("http://127.0.0.1:8082/two")
+		resp, err := http.Get(srvTwoURL)
 		if err != nil {
 			fmt.Printf("Error accessing service 2\n%v", err)
 			fmt.Fprint(w, err)
